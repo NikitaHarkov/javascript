a15b47ca1073ebca74f799039615def28e96b062
«Nikita <mrnikita.harkov@gmail.com>
2021-01-21 14:11:40 +0200

    Row and Column resize component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,18 @@ import {createTable} from './table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
   toHtml() {
     return createTable(20)
+  }
+
+  onMousedown(event) {
+    // console.log(event.target.getAttribute('data-resize'));
+    if (event.target.dataset.resize);
   }
 }
